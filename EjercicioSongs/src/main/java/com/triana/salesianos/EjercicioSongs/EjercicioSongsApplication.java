75bb46c01e2d6294edee9f1ad180615c85b329e1
Daniel-DLR <danidlr02@gmail.com>
2021-10-25 18:14:49 +0200

    First commit

---
+++ EjercicioSongs/src/main/java/com/triana/salesianos/EjercicioSongs/EjercicioSongsApplication.java
@@ -0,0 +1,13 @@
+package com.triana.salesianos.EjercicioSongs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioSongsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioSongsApplication.class, args);
+	}
+
+}
---
+++ EjercicioSongs/src/test/java/com/triana/salesianos/EjercicioSongs/EjercicioSongsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triana.salesianos.EjercicioSongs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioSongsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
